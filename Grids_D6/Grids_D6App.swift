812293eb506c8e4546466f0a2d9d36c9ef93b431
sirx2713 <bradx2713@gmail.com>
2024-03-16 19:24:03 +0300

    Initial Commit

---
+++ Grids_D6/Grids_D6App.swift
@@ -0,0 +1,17 @@
+//
+//  Grids_D6App.swift
+//  Grids_D6
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Grids_D6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
